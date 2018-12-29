ff527f2f155c353c91ae8b8b05964fc08c25a2bf
Julian Chan <julianmkchan@gmail.com>
2018-12-29 21:39:24 +0800

    add process.envport

--- app.js
+++ app.js
@@ -48,6 +48,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("YelpCamp server has started");
 });
